b9ebe3399a79ab15f4744592aaa6630674bd5f28
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-02-10 18:41:52 +0300

    Получили результат с именованным параметром base

--- 0210_WEB4/ex3.py
+++ 0210_WEB4/ex3.py
@@ -3,6 +3,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("integers", metavar="Целые числа", nargs='+',
                     type=str, help="Перевод двоичных чисел в десятичные")
+parser.add_argument("--base", default=2, type=int,
+                    help="Основание системы счисления")
 args = parser.parse_args()
-result = " ".join(map(lambda x: str(int(x, 2)), args.integers))
+result = " ".join(map(lambda x: str(int(x, args.base)), args.integers))
 print(result)
