1c084520f73d47a96295e88b741024505aa344ef
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-02-10 18:47:27 +0300

    Закончили эксперимент 3.

--- 0210_WEB4/ex3.py
+++ 0210_WEB4/ex3.py
@@ -1,10 +1,14 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("integers", metavar="Целые числа", nargs='+',
                     type=str, help="Перевод двоичных чисел в десятичные")
 parser.add_argument("--base", default=2, type=int,
                     help="Основание системы счисления")
+parser.add_argument("--log", default=sys.stdout, type=argparse.FileType('a'),
+                    help="Запись результата в лог-файл")
 args = parser.parse_args()
 result = " ".join(map(lambda x: str(int(x, args.base)), args.integers))
-print(result)
+args.log.write(result + '\n')
+args.log.close()
