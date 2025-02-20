b8e1da98d2b9040a1f8bc45dc28a1fdb6f124e5c
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-02-20 19:17:44 +0300

    Счетчик посещений

--- 0220_WEB7 flask1/main_server2.py
+++ 0220_WEB7 flask1/main_server2.py
@@ -51,14 +51,21 @@ def bootstrap():
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>А что это вы тут делаете?</h1>
     <div class="alert alert-success" role="alert">
-  A simple success alert—check it out!
+  А мы тут элементами bootstrap балуемся!
 </div>
+<button type="button" class="btn btn-warning">Warning</button>
 </body>
 </html>"""
 
-
+i = 0
+@app.route('/show_i')
+def show_i():
+    # http://127.0.0.1:8080/show_i
+    global i
+    i += 1
+    return f"Посещение: {i}"
 
 
 if __name__ == '__main__':
