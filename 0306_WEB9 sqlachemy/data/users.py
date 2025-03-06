6cceaa92052390d256c4b8070150b0d855fd5697
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-06 18:54:18 +0300

    Создали таблицу users

---
+++ 0306_WEB9 sqlachemy/data/users.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+from datetime import datetime
+from .db_session import SqlAlchemyBase
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, nullable=True)
+    about = sa.Column(sa.String, nullable=True)
+    email = sa.Column(sa.String, nullable=True, unique=True, index=True)
+    hashed_password = sa.Column(sa.String, nullable=True)
+    created_data = sa.Column(sa.DateTime, default=datetime.now())
