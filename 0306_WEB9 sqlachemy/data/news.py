86d8120bd5291cf661e1008d318d98dac7c1865c
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-06 19:45:53 +0300

    Создали форму пока без правильного пароля

--- 0306_WEB9 sqlachemy/data/news.py
+++ 0306_WEB9 sqlachemy/data/news.py
@@ -16,3 +16,6 @@ class News(SqlAlchemyBase):
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
+
+    def __repr__(self):
+        return f"<User> {self.id} {self.title} {self.content}"
