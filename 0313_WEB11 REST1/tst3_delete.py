3bf4ebb7bf942d0c10d4df3fddc66d8007b4d335
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-13 19:08:49 +0300

    Создан запрос DELETE

---
+++ 0313_WEB11 REST1/tst3_delete.py
@@ -0,0 +1,7 @@
+from requests import delete
+
+
+print(delete('http://localhost:8080/api/news/999').json())
+# новости с id = 999 нет в базе
+
+print(delete('http://localhost:8080/api/news/1').json())
