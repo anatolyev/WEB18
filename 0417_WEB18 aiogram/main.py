1c1d3150148edb42947ac623cfbbed89b6980819
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-17 18:35:58 +0300

    Создали бота с клавиатурой aiogram

--- 0417_WEB18 aiogram/main.py
+++ 0417_WEB18 aiogram/main.py
@@ -4,17 +4,57 @@ import sys
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 # Bot token can be obtained via https://t.me/BotFather
 from config import BOT_TOKEN
 
 # Клавиатура
-
+reply_keyboard = [[KeyboardButton(text='/address'), KeyboardButton(text='/phone')],
+                  [KeyboardButton(text='/site'), KeyboardButton(text='/work_time')],
+                  [KeyboardButton(text='/stop')],
+                  ]
+markup = ReplyKeyboardMarkup(keyboard=reply_keyboard,
+                             one_time_keyboard=False,
+                             resize_keyboard=True)
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start(message: Message) -> None:
+    await message.reply(f"Привет! Смотри клавиатуру снизу...", reply_markup=markup)
+
+
+@dp.message(Command('stop'))
+async def stop(message: Message) -> None:
+    await message.reply(f"Пока! Клавиатура была закрыта.", reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message(Command('help'))
+async def help(message: Message) -> None:
+    await message.reply(f"тут нам когданибудь помогут.")
+
+@dp.message(Command('address'))
+async def address(message: Message) -> None:
+    await message.reply(f"Адрес компании")
+
+@dp.message(Command('phone'))
+async def phone(message: Message) -> None:
+    await message.reply(f"Телефон: 123-45-67")
+
+
+@dp.message(Command('site'))
+async def site(message: Message) -> None:
+    await message.reply(f"Сайт компании")
+
+
+@dp.message(Command('work_time'))
+async def work_time(message: Message) -> None:
+    await message.reply(f"Рабочее время: бесконечно")
+
 
 
 @dp.message(CommandStart())
