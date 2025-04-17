eea4d767ce93a541747f1e828c967db9caedf415
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-17 19:37:02 +0300

    Создали бота с отправкой изображений aiogram

--- 0417_WEB18 aiogram/main.py
+++ 0417_WEB18 aiogram/main.py
@@ -5,7 +5,9 @@ from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, FSInputFile
+from aiogram.utils.chat_action import ChatActionSender
+from aiogram.utils.media_group import MediaGroupBuilder
 
 # Bot token can be obtained via https://t.me/BotFather
 from config import BOT_TOKEN
@@ -13,6 +15,7 @@ from config import BOT_TOKEN
 # Клавиатура
 reply_keyboard = [[KeyboardButton(text='/address'), KeyboardButton(text='/phone')],
                   [KeyboardButton(text='/site'), KeyboardButton(text='/work_time')],
+                  [KeyboardButton(text='/picture')],
                   [KeyboardButton(text='/stop')],
                   ]
 markup = ReplyKeyboardMarkup(keyboard=reply_keyboard,
@@ -21,6 +24,27 @@ markup = ReplyKeyboardMarkup(keyboard=reply_keyboard,
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
+
+
+@dp.message(Command('picture'))
+async def picture(message: Message) -> None:
+    await message.reply(f"Вы хотите увидеть изображения в нашем боте?")
+    await asyncio.sleep(1)
+
+    async with ChatActionSender.upload_photo(bot=message.bot, chat_id=message.chat.id):
+        media = MediaGroupBuilder(caption="Заголовок для группы картинок!")
+        media.add_photo(FSInputFile("static/images/owl1.jpg"), 'Главная сова Яндекс.Лицея!')
+        await asyncio.sleep(1)
+        await message.answer(f"Мы готовим, что-то интересное для Вас...")
+        media.add_photo(FSInputFile("static/images/owl2.png"), 'Просто сова.')
+        media.add_photo(FSInputFile("static/images/el.jpeg"), 'Купи слона!')
+        await asyncio.sleep(1)
+        media.add_photo('http://cataas.com/cat/hello', 'Случайный кот!')
+        await asyncio.sleep(2)
+        await message.answer_media_group(media=media.build())
+
+
+
 
 
 @dp.message(Command('start'))
